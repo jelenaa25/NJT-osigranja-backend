807314537605724b4fec3f1968ae71bda1b5c9eb
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-28 20:30:05 +0200

    Inicijalizovana Spring boot aplikacija

---
+++ src/main/java/rs/ac/bg/fon/osiguranja/OsiguranjaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.osiguranja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsiguranjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsiguranjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/osiguranja/OsiguranjaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.osiguranja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsiguranjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
