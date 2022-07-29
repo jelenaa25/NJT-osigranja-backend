2808e7c4a336ac98a2bae57e306ac2c80a849f28
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-29 23:31:48 +0200

    Napravljen repository

---
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/Controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.osiguranja.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Korisnik
+ */
+@RestController
+
+public class Controller {
+    @GetMapping("/vrati")
+    public int vratiJedan(){
+        return 22;
+    }
+}
---
+++ src/main/java/rs/ac/bg/fon/osiguranja/repository/PolisaRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.osiguranja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.bg.fon.osiguranja.model.Polisa;
+
+/**
+ *
+ * @author Korisnik
+ */
+@Repository
+public interface PolisaRepository extends JpaRepository<Polisa, Integer>{
+    
+}
