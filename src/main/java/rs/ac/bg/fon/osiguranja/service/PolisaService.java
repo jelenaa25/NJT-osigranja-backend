1c5d8c309d352d18c8844445567bdbef37b3ac6a
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-30 10:41:32 +0200

    kreirane nove metode

--- src/main/java/rs/ac/bg/fon/osiguranja/service/PolisaService.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/service/PolisaService.java
@@ -5,8 +5,10 @@
  */
 package rs.ac.bg.fon.osiguranja.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rs.ac.bg.fon.osiguranja.model.Polisa;
 import rs.ac.bg.fon.osiguranja.repository.PolisaRepository;
 
 /**
@@ -17,4 +19,16 @@ import rs.ac.bg.fon.osiguranja.repository.PolisaRepository;
 public class PolisaService {
     @Autowired
     private PolisaRepository polisaRepository;
+
+    public Polisa kreirajPolisu(Polisa p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Polisa> vratiSvePolise() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean obrisiPolisu(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
