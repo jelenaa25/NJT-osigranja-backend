5341aea6c4b3a0f60f26df768f94c44ff227ede3
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-30 10:41:05 +0200

    dodate metode

--- src/main/java/rs/ac/bg/fon/osiguranja/controller/PolisaController.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/PolisaController.java
@@ -5,8 +5,15 @@
  */
 package rs.ac.bg.fon.osiguranja.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import rs.ac.bg.fon.osiguranja.model.Polisa;
 import rs.ac.bg.fon.osiguranja.service.PolisaService;
 
 /**
@@ -17,4 +24,21 @@ import rs.ac.bg.fon.osiguranja.service.PolisaService;
 public class PolisaController {
     @Autowired
     private PolisaService polisaService;
+    
+    @PostMapping("/polisa")
+    public Polisa kreirajPolisu(@RequestBody Polisa p) throws Exception{
+        return polisaService.kreirajPolisu(p);
+    }
+    
+    @GetMapping("/polisa")
+    public List<Polisa> vratiSvePolise()throws Exception{
+      return polisaService.vratiSvePolise();
+    }
+    
+    @DeleteMapping("/polisa{id}")
+    public boolean obrisiPolisu(@PathVariable("id") int id ) throws Exception{
+        return polisaService.obrisiPolisu(id);
+    }
+    
+    
 }
