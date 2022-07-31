d59461a519590aa3c913a9c3a44af0fe284d21ae
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-31 23:10:26 +0200

    method={RequestMethod.DELETE, RequestMethod.GET} ne radi

--- src/main/java/rs/ac/bg/fon/osiguranja/controller/PolisaController.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/PolisaController.java
@@ -11,9 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import rs.ac.bg.fon.osiguranja.dto.PolisaDto;
 import rs.ac.bg.fon.osiguranja.model.Polisa;
@@ -46,8 +49,7 @@ public class PolisaController {
     public List<PolisaDto> vratiSvePolise()throws Exception{
       return polisaService.vratiSvePolise();
     }
-    
-    @DeleteMapping("/polisa{id}")
+    @DeleteMapping("/polisa/{id}")
     public ResponseEntity<Object> obrisiPolisu(@PathVariable("id") int id ) throws Exception{
         
         try {
