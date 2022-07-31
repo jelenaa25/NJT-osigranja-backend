79e67a7583a15fdfd196e4506927678affb653b8
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-31 18:49:58 +0200

    obrisani nepotrebni importi

--- src/main/java/rs/ac/bg/fon/osiguranja/model/Polisa.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/model/Polisa.java
@@ -22,19 +22,13 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 /**
  *
  * @author Korisnik
  */
 @Data
-@ToString
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
