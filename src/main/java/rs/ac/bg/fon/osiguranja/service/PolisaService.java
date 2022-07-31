ff255d803becf9786503f6321fccdc55c5d4aae2
Jelena Beganovic <beganovicjelena@gmail.com>
2022-07-31 18:51:29 +0200

    izmena metode

--- src/main/java/rs/ac/bg/fon/osiguranja/service/PolisaService.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/service/PolisaService.java
@@ -5,11 +5,16 @@
  */
 package rs.ac.bg.fon.osiguranja.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import rs.ac.bg.fon.osiguranja.dto.PolisaDto;
+import rs.ac.bg.fon.osiguranja.mapper.PolisaMapper;
 import rs.ac.bg.fon.osiguranja.model.Polisa;
 import rs.ac.bg.fon.osiguranja.repository.PolisaRepository;
 
@@ -18,25 +23,39 @@ import rs.ac.bg.fon.osiguranja.repository.PolisaRepository;
  * @author Korisnik
  */
 @Service
-@Transactional
 public class PolisaService {
-    @Autowired
+    
+   
     private PolisaRepository polisaRepository;
+    private PolisaMapper polisaMapper;
 
-    public Polisa kreirajPolisu(Polisa p) {
-
-        //validacija TODO
+    public PolisaService(PolisaRepository polisaRepository, PolisaMapper polisaMapper) {
+        this.polisaRepository = polisaRepository;
+        this.polisaMapper = polisaMapper;
+    }
+    
+    @Transactional
+    public PolisaDto kreirajPolisu(PolisaDto p) {
+        return polisaMapper.toDto(polisaRepository.save(polisaMapper.toEntity(p)));
+    }
+    
+    @Transactional
+    public List<PolisaDto> vratiSvePolise() {
+        List<Polisa> p = polisaRepository.findAll();
+        System.out.println("POLISE: "+p.size());
+        if(p.get(0).getDatumOD() == null){
+            System.out.println("NULLLL JEEEE...............................................");
+        }
+        return p.stream().map((pp) -> {
+            return polisaMapper.toDto(pp);
+        }).collect(Collectors.toList());
         
-        return polisaRepository.save(p);
-
+      
+       // return polisaRepository.findAll();
     }
 
-    public List<Polisa> vratiSvePolise() {
-        return polisaRepository.findAll();
-    }
-
+    @Transactional
     public boolean obrisiPolisu(int id) throws Exception{
-
            polisaRepository.deleteById(id);
            return true;
     }
