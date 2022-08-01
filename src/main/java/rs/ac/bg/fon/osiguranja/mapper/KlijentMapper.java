57796c35bfb075107997a4e33fd9028253a0a088
Jelena Beganovic <beganovicjelena@gmail.com>
2022-08-01 18:47:42 +0200

    promenjen mapper za klijenta

--- src/main/java/rs/ac/bg/fon/osiguranja/mapper/KlijentMapper.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/mapper/KlijentMapper.java
@@ -4,32 +4,41 @@
  * and open the template in the editor.
  */
 package rs.ac.bg.fon.osiguranja.mapper;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import rs.ac.bg.fon.osiguranja.dto.KlijentDto;
 import rs.ac.bg.fon.osiguranja.model.Klijent;
+import rs.ac.bg.fon.osiguranja.model.Mesto;
+import rs.ac.bg.fon.osiguranja.repository.MestoRepository;
 
 /**
  *
  * @author Korisnik
  */
 @Component
+@AllArgsConstructor
 public class KlijentMapper implements GenericMapper<KlijentDto, Klijent>{
 //dodaj mestoRepositoryyy
+    private MestoRepository mestoRepository;
+    
     @Override
     public Klijent toEntity(KlijentDto dto) {
         Klijent k = new Klijent();
-        k.setId(dto.getId());
+        k.setJmbg(dto.getJmbg());
         k.setImePrezime(dto.getImePrezime());
-        //k.setMesto();
+        Mesto mm;
+        mm = mestoRepository.findByNaziv(dto.getMesto());
+        //k.setMesto(new Mesto(11000, "Beograd"));
+        k.setMesto(mm);
         return k;
     }
 
     @Override
     public KlijentDto toDto(Klijent entity) {
         KlijentDto k = new KlijentDto();
-        k.setId(entity.getId());
+        k.setJmbg(entity.getJmbg());
         k.setImePrezime(entity.getImePrezime());
-        k.setMesto(entity.getMesto().getPTT());
+        k.setMesto(entity.getMesto().getNaziv());
         return k;
     }
     
