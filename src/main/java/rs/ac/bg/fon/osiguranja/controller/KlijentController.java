e65f44bf29c8631881057f39a41ecf411aaca018
Jelena Beganovic <beganovicjelena@gmail.com>
2022-08-01 18:46:51 +0200

    izmenjen kontroler

--- src/main/java/rs/ac/bg/fon/osiguranja/controller/KlijentController.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/KlijentController.java
@@ -5,10 +5,36 @@
  */
 package rs.ac.bg.fon.osiguranja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import rs.ac.bg.fon.osiguranja.dto.KlijentDto;
+import rs.ac.bg.fon.osiguranja.dto.PolisaDto;
+import rs.ac.bg.fon.osiguranja.service.KlijentService;
+
 /**
  *
  * @author Korisnik
  */
+@RestController
 public class KlijentController {
+    private final KlijentService klijentService;
+
+    public KlijentController(KlijentService klijentService) {
+        this.klijentService = klijentService;
+    }
+    
+    @PostMapping("/klijent")
+    public ResponseEntity<Object> kreirajKlijenta(@RequestBody KlijentDto p) throws Exception{
+        try {
+            System.out.println("KLDTOOO:  "+p);
+            return ResponseEntity.ok(klijentService.kreirajKlijenta(p));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
+    }
     
 }
