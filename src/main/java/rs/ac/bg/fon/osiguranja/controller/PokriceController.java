8b177e5d86f8d5e003ec4e263490d5dc4b5d1313
Jelena Beganovic <beganovicjelena@gmail.com>
2022-08-06 07:37:42 +0200

    dodat komentar za ucitavanje pokrica

--- src/main/java/rs/ac/bg/fon/osiguranja/controller/PokriceController.java
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/PokriceController.java
@@ -5,10 +5,64 @@
  */
 package rs.ac.bg.fon.osiguranja.controller;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import rs.ac.bg.fon.osiguranja.dto.PokriceDto;
+import rs.ac.bg.fon.osiguranja.dto.PolisaDto;
+import rs.ac.bg.fon.osiguranja.service.PokriceService;
+
 /**
  *
  * @author Korisnik
  */
+@RestController
 public class PokriceController {
-    
+
+    private final PokriceService pokriceService;
+
+    public PokriceController(PokriceService pokriceService) {
+        this.pokriceService = pokriceService;
+    }
+
+    @GetMapping("/pokrice")
+    public List<PokriceDto> vratiSvaPokrica() throws Exception {
+        return pokriceService.vratiSvaPokrica();
+    }
+
+    @PostMapping("/pokrice")
+    public ResponseEntity<Object> kreirajPokrice(@RequestBody PokriceDto p) throws Exception {
+        try {
+            return ResponseEntity.ok(pokriceService.kreirajPokrice(p));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("pokrice/{id}")
+    public ResponseEntity<Object> nadjiPokrice(@PathVariable int id) { //vrati odredjeno pokrice
+        Optional<PokriceDto> entity = pokriceService.nadjiPokrice(id);
+        if (entity.isPresent()) {
+            return ResponseEntity.ok(entity.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nepostojece pokrice...");
+    }
+
+    @PutMapping("pokrice/{id}")
+    public ResponseEntity<Object> promeniPokriceById(@PathVariable("id") int id,
+            @RequestBody PokriceDto pok) throws Exception {
+        try {
+            return ResponseEntity.ok(pokriceService.promeniPokriceById(id, pok));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+
+        }
+    }
 }
