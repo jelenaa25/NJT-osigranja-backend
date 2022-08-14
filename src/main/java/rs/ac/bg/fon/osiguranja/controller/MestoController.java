618ad056a3d4f94abfff0127ed024799481c9d83
Jelena Beganovic <beganovicjelena@gmail.com>
2022-08-14 15:44:38 +0200

    vracanje liste mesta

---
+++ src/main/java/rs/ac/bg/fon/osiguranja/controller/MestoController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.osiguranja.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.ac.bg.fon.osiguranja.dto.KlijentDto;
+import rs.ac.bg.fon.osiguranja.model.Mesto;
+import rs.ac.bg.fon.osiguranja.repository.MestoRepository;
+
+/**
+ *
+ * @author Korisnik
+ */
+@RestController
+public class MestoController {
+    private final MestoRepository mestoRepository;
+
+    public MestoController(MestoRepository mestoRepository) {
+        this.mestoRepository = mestoRepository;
+    }
+    
+    @GetMapping("/mesto")
+    public List<Mesto> vratiMesta() throws Exception {
+        return mestoRepository.findAll();
+    }
+}
